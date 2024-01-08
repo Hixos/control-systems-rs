54de95dd33a952f88a4c3859c33461ddae9472e6
Luca Erbetta <luca.erbetta105@gmail.com>
2024-01-08 19:49:22 +0100

    Update example

--- control-system-blocks/examples/add.rs
+++ control-system-blocks/examples/add.rs
@@ -1,27 +1,31 @@
+use std::collections::btree_map::Iter;
+
 use anyhow::Result;
 use control_system::ControlSystemBuilder;
-use control_system::{Block, BlockIO};
 
-use control_system_blocks::{Add, Constant, Print};
+use control_system_blocks::{Add, Constant, Print, Delay};
 
 fn main() -> Result<()> {
     let add = Add::<i32, 2>::new("add");
 
     let print = Print::<i32>::new("print");
 
-    let c1 = Constant::<i32>::new("const_1", 3);
-    let c2 = Constant::<i32>::new("const_2", 4);
+    let c1 = Constant::<i32>::new("const_1", 1);
+    let delay = Delay::<i32, 1>::new("delay", [0]);
 
     let mut builder = ControlSystemBuilder::default();
 
-    builder.add_block(c1, &[], &[("y", "c1")])?;
-    builder.add_block(c2, &[], &[("y", "c2")])?;
-    builder.add_block(add, &[("u1", "c1"), ("u2", "c2")], &[("y", "sum")])?;
+    builder.add_block(c1, &[], &[("y", "one")])?;
+    builder.add_block(delay, &[("u", "sum")], &[("y", "feedback")])?;
+
+    builder.add_block(add, &[("u1", "one"), ("u2", "feedback")], &[("y", "sum")])?;
     builder.add_block(print, &[("u", "sum")], &[])?;
 
-    let mut controlsystem = builder.build()?;
+    let mut controlsystem = builder.build(1.0)?;
 
-    controlsystem.step();
+    for _ in 0..10 {
+        controlsystem.step();
+    }
 
     Ok(())
 }
