69c46cce9621402419be487f70a5313f9a70fa89
Luca Erbetta <luca.erbetta105@gmail.com>
2024-01-16 00:53:09 +0100

    Added examples crate

---
+++ control_system_examples/examples/cart_plot.rs
@@ -0,0 +1,124 @@
+use std::{
+    sync::mpsc::{channel, Sender},
+    thread::spawn,
+};
+
+use anyhow::Result;
+use control_system::{
+    controlblock::StepInfo,
+    io::{Input, Output},
+    numeric::ode::{ODESolver, RungeKutta4},
+    Block,
+};
+use control_system::{BlockIO, ControlSystemBuilder};
+use control_system_blocks::Constant;
+use control_system_derive::BlockIO;
+use control_system_plotter::Plotter;
+use nalgebra::Vector2;
+use rust_data_inspector::datainspector::DataInspector;
+use rust_data_inspector_signals::PlotSignals;
+
+#[derive(BlockIO)]
+struct Cart {
+    #[blockio(block_name)]
+    name: String,
+
+    #[blockio(input)]
+    u_force: Input<f64>,
+
+    #[blockio(output)]
+    y_pos: Output<f64>,
+    #[blockio(output)]
+    y_vel: Output<f64>,
+    #[blockio(output)]
+    y_acc: Output<f64>,
+
+    mass: f64,
+    state: Vector2<f64>,
+}
+
+impl Block for Cart {
+    fn step(&mut self, k: StepInfo) {
+        let acc = self.u_force.get() / self.mass;
+
+        let odefun = |_, x: Vector2<f64>| Vector2::new(x[1], acc);
+
+        // Propagate state
+        self.state = RungeKutta4::solve(odefun, k.t, k.dt, self.state);
+
+        // Assign outputs
+        self.y_pos.set(self.state[0]);
+        self.y_vel.set(self.state[1]);
+        self.y_acc.set(acc);
+    }
+}
+
+impl Cart {
+    fn new(mass: f64, pos0: f64, vel0: f64) -> Self {
+        Cart {
+            name: "cart".to_string(),
+            mass,
+            state: Vector2::new(pos0, vel0),
+            u_force: Input::default(),
+            y_pos: Output::default(),
+            y_vel: Output::default(),
+            y_acc: Output::default(),
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    let (signals_snd, signals_rcv) = channel();
+
+    spawn(move || {
+        run_control_system(signals_snd).unwrap();
+    });
+
+    let signals = signals_rcv.recv().expect("Error receiving signals");
+
+    DataInspector::run_native("Cart control system", signals).expect("Error");
+    Ok(())
+}
+
+fn run_control_system(signals_snd: Sender<PlotSignals>) -> Result<()> {
+    let mut signals = PlotSignals::default();
+
+    let cart = Cart::new(1.0, 0.0, 0.0);
+    let constant = Constant::new("force", 100.0);
+
+    let plot_pos = Plotter::<f64>::new("plot_pos", "/cart/pos", &mut signals);
+    let plot_vel = Plotter::<f64>::new("plot_vel", "/cart/vel", &mut signals);
+    let plot_acc = Plotter::<f64>::new("plot_acc", "/cart/acc", &mut signals);
+    let plot_force = Plotter::<f64>::new("plot_force", "/cart/force", &mut signals);
+
+    // drop(signals);
+
+    let mut builder = ControlSystemBuilder::default();
+
+    builder.add_block(
+        cart,
+        &[("u_force", "/force")],
+        &[
+            ("y_pos", "/cart/pos"),
+            ("y_vel", "/cart/vel"),
+            ("y_acc", "/cart/acc"),
+        ],
+    )?;
+
+    builder.add_block(constant, &[], &[("y", "/force")])?;
+    builder.add_block(plot_pos, &[("u", "/cart/pos")], &[])?;
+    builder.add_block(plot_vel, &[("u", "/cart/vel")], &[])?;
+    builder.add_block(plot_acc, &[("u", "/cart/acc")], &[])?;
+    builder.add_block(plot_force, &[("u", "/force")], &[])?;
+
+    // Signals are properly populated. Send them to be used by the GUI
+    signals_snd.send(signals).expect("Could not send signals to GUI");
+
+    let mut cs = builder.build(0.01)?;
+
+    for _ in 0..1000 {
+        cs.step();
+    }
+
+    Ok(())
+}
