530d182b0a8335200c6fe68be87ae0fb20ce981f
Luca Erbetta <luca.erbetta105@gmail.com>
2024-01-16 00:51:54 +0100

    Added ode functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod controlblock;
 pub mod controlsystem;
 pub mod io;
+pub mod numeric;
 
 pub use controlblock::{BlockIO, Block};
 pub use  controlsystem::{ControlSystem, ControlSystemBuilder};
