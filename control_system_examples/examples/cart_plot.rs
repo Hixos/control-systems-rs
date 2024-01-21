f6e82aad7c45d457d58a51ede32318e5d5e18f68
Luca Erbetta <luca.erbetta105@gmail.com>
2024-01-21 19:22:02 +0100

    Updated example with nested loop & pids

--- control_system_examples/examples/cart_plot.rs
+++ control_system_examples/examples/cart_plot.rs
@@ -11,8 +11,10 @@ use control_system::{
     Block, ControlSystemParameters, ParameterStore, StepInfo, StepResult,
 };
 use control_system::{BlockIO, ControlSystemBuilder};
-use control_system_blocks::{Constant, ConstantParams};
-use control_system_derive::BlockIO;
+use control_system_blocks::{
+    siso::{PIDParams, PID},
+    Add, Constant, Delay,
+};
 use control_system_plotter::add_plotter;
 use nalgebra::Vector2;
 use rust_data_inspector::datainspector::DataInspector;
@@ -99,23 +101,18 @@ fn main() -> Result<()> {
 fn run_control_system(signals_snd: Sender<PlotSignals>) -> Result<()> {
     let mut store = ParameterStore::new(Path::new("cart.toml"), "cart")?;
 
-    let mut signals = PlotSignals::default();
-
-    let cart = Cart::from_store(
-        &mut store,
-        CartParams {
-            mass: 1.0,
-            pos0: 0.0,
-            vel0: 0.0,
-        },
-    )?;
-
-    let constant = Constant::from_store("force", &mut store, ConstantParams { c: 100.0 })?;
-
     let mut builder = ControlSystemBuilder::default();
 
+    // Cart
     builder.add_block(
-        cart,
+        Cart::from_store(
+            &mut store,
+            CartParams {
+                mass: 1.0,
+                pos0: 0.0,
+                vel0: 0.0,
+            },
+        )?,
         &[("u_force", "/force")],
         &[
             ("y_pos", "/cart/pos"),
@@ -124,23 +121,99 @@ fn run_control_system(signals_snd: Sender<PlotSignals>) -> Result<()> {
         ],
     )?;
 
-    builder.add_block(constant, &[], &[("y", "/force")])?;
+    // ** Inner loop **
+    // Inputs:
+    // - /ref/vel: Velocity reference
+    // Outputs:
+    // - /force: Commanded force on the cart
 
+    builder.add_block(
+        Delay::from_store("vel_delay", &mut store, [0.0].into())?,
+        &[("u", "/cart/vel")],
+        &[("y", "/cart/vel_delayed")],
+    )?;
+
+    builder.add_block(
+        Add::<f64, 2>::new("vel_err", [1.0, -1.0].into()),
+        &[("u1", "/ref/vel"), ("u2", "/cart/vel_delayed")],
+        &[("y", "/err/vel")],
+    )?;
+
+    builder.add_block(
+        PID::from_store(
+            "pid_vel",
+            &mut store,
+            PIDParams {
+                kp: 4.0,
+                ..Default::default()
+            },
+        )?,
+        &[("u", "/err/vel")],
+        &[("y", "/force")],
+    )?;
+
+    // ** Outer Loop **
+    // Inputs:
+    // - /ref/pos: Position reference
+    // Outputs:
+    // - /ref/vel: Velocity reference
+
+    builder.add_block(
+        Delay::from_store("pos_delay", &mut store, [0.0].into())?,
+        &[("u", "/cart/pos")],
+        &[("y", "/cart/pos_delayed")],
+    )?;
+
+    builder.add_block(
+        Add::<f64, 2>::new("pos_err", [1.0, -1.0].into()),
+        &[("u1", "/ref/pos"), ("u2", "/cart/pos_delayed")],
+        &[("y", "/err/pos")],
+    )?;
+
+    builder.add_block(
+        PID::from_store(
+            "pid_pos",
+            &mut store,
+            PIDParams {
+                kp: 1.0,
+                ..Default::default()
+            },
+        )?,
+        &[("u", "/err/pos")],
+        &[("y", "/ref/vel")],
+    )?;
+
+    // Position reference
+
+    builder.add_block(
+        Constant::from_store("pos_ref", &mut store, 15.0.into())?,
+        &[],
+        &[("y", "/ref/pos")],
+    )?;
+
+    // Plotters
+    let mut signals = PlotSignals::default();
     add_plotter::<f64>("/cart/pos", &mut builder, &mut signals)?;
     add_plotter::<f64>("/cart/vel", &mut builder, &mut signals)?;
     add_plotter::<f64>("/cart/acc", &mut builder, &mut signals)?;
     add_plotter::<f64>("/force", &mut builder, &mut signals)?;
+    add_plotter::<f64>("/ref/pos", &mut builder, &mut signals)?;
+    add_plotter::<f64>("/ref/vel", &mut builder, &mut signals)?;
+    add_plotter::<f64>("/err/pos", &mut builder, &mut signals)?;
+    add_plotter::<f64>("/err/vel", &mut builder, &mut signals)?;
+
+    // Build the control system
+    let mut cs =
+        builder.build_from_store("cart", &mut store, ControlSystemParameters::new(0.01))?;
+
+    store.save()?;
 
     // Signals are properly populated. Send them to be used by the GUI
     signals_snd
         .send(signals)
         .expect("Could not send signals to GUI");
 
-    let mut cs =
-        builder.build_from_store("cart", &mut store, ControlSystemParameters::new(0.01))?;
-
-    store.save()?;
-
+    // Execute
     for _ in 0..1000 {
         if cs.step()? == StepResult::Stop {
             break;
